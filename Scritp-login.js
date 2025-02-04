e9fe131ae87fcb98e2ad1e31cef225ccc4021718
Be-type <Bernardoboechat6@gmail.com>
2025-02-04 00:06:58 -0300

    Atualização no código

---
+++ Scritp-login.js
@@ -0,0 +1,16 @@
+function openConfig() {
+    document.getElementById('carrinho').style.display = 'block';
+  }
+  
+  function closeConfig() {
+    document.getElementById('carrinho').style.display = 'none';
+  }
+  
+  
+  function openMenu() {
+    document.getElementById('login').style.display = 'block';
+  }
+  
+  function closeMenu() {
+    document.getElementById('login').style.display = 'none';
+  }
--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-function openConfig() {
-  document.getElementById('carrinho').style.display = 'block';
-}
-
-function closeConfig() {
-  document.getElementById('carrinho').style.display = 'none';
-}
-
-
-function openMenu() {
-  document.getElementById('login').style.display = 'block';
-}
-
-function closeMenu() {
-  document.getElementById('login').style.display = 'none';
-}
-
 let currentIndex = 0; // Índice atual da imagem
 const sliderContainer = document.getElementById('sliderContainer');
 const totalImages = sliderContainer.children.length; // Número total de imagens no slider
